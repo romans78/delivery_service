1c288d2412dc0829def438049f2e2ad4087ac257
roman <roman.sizov@gmail.com>
2025-05-29 01:20:07 +0300

    added endpoint

--- endpoints/deliveries.py
+++ endpoints/deliveries.py
@@ -1,8 +1,18 @@
 from fastapi import APIRouter, Depends, Query
 from fastapi.responses import Response
 
+from models.packages import PackageCreate, PackageId
+
 router = APIRouter(prefix='/api/v1', tags=['deliveries'])
+
 
 @router.get('/deliveries/test')
 async def test(msg: int):
     return {"message": msg}
+
+
+@router.post('/package',
+             response_model=PackageId,
+             description='This method registers a package')
+async def register_package(package: PackageCreate):
+    return PackageId(id=str(package.content_value_usd))
